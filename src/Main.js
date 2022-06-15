a32e15caf62cb0bcc1facdfd590b22b6734d615d
Katerina S <kslezakova12@gmail.com>
2022-06-15 08:47:23 +0200

    html structure + css styling

---
+++ src/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" id="title" autoFocus />
+        <textarea id="body" placeholder="Type your note here..." >
+
+        </textarea>
+      </div>
+      <div className="app-main-note-preview">
+<h1 className="preview-title">
+  TITLE
+</h1>
+<div className="markdown-preview">
+  note preview
+</div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
